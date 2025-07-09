b8a16354d2efe0642c9753b4784c60326da8b6a9
Ananya Singh <singhananya5040@gmail.com>
2025-07-09 09:27:19 +0530

    09-07-25

--- Trees/buildOne.cpp
+++ Trees/buildOne.cpp
@@ -12,7 +12,27 @@ class Node{
         this->right = NULL;
     }
 };
+Node* buildTree()
+{
+    cout<<"Enter data: "<<endl;
+    int data;
+    cin>>data;
+    if(data == -1)
+    {
+        return NULL;
+    }
+    Node* root = new Node(data);
+    cout<<"Enter left node data: "<<endl;
+    root->left = buildTree();
+    cout<<"Enter right node data: "<<endl;
+    root->right = buildTree();
+}
 int main()
 {
+
+    Node* root = NULL;
+    root = buildTree();
+    cout<<"Tree built succesfully!"<<endl;
+
     return 0;
 }
