8d781dc2e1c5393c57551fb150377652309f1848
Ananya Singh <singhananya5040@gmail.com>
2025-07-09 09:38:24 +0530

    09-07-25

---
+++ Trees/LevelOrder.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        this->data = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* buildTree()
+{
+    cout<<"Enter data: "<<endl;
+    int data;
+    cin>>data;
+    if(data == -1)
+    {
+        return NULL;
+    }
+    Node* root = new Node(data);
+    cout<<"Enter left node data: "<<endl;
+    root->left = buildTree();
+    cout<<"Enter right node data: "<<endl;
+    root->right = buildTree();
+    return root;
+}
+
+int main()
+{
+
+    Node* root = NULL;
+    root = buildTree();
+    cout<<"Tree built succesfully!"<<endl;
+
+    return 0;
+}
