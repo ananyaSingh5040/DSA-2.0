f7e47a6b6d8c316246d52de939946559a01b4529
Ananya Singh <singhananya5040@gmail.com>
2025-08-13 21:07:11 +0530

    13-08-25

--- HashMap/implementation.cpp
+++ HashMap/implementation.cpp
@@ -5,5 +5,7 @@ int main()
     unordered_map<string,int> mpp;
     pair <string,int> a = make_pair ("love", 2);
     mpp.insert(a);
+    pair<string, int> b ("babbar",3);
+    mpp.insert(b);
     return 0;
 }
